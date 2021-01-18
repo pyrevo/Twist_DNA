7daaedc7f22f164249c39607eb5750fc1535a8b1
Patrik Smeds <patrik.smeds@gmail.com>
2021-01-18 10:12:49 +0100

    Make sure python scripts follows pycodestyle.

--- src/Snakemake/scripts/hardFilter_PASS_mutect2.py
+++ src/Snakemake/scripts/hardFilter_PASS_mutect2.py
@@ -3,7 +3,7 @@ import sys
 from pysam import VariantFile
 
 vcf_in = VariantFile(sys.argv[1])
-vcf_out = VariantFile(sys.argv[2], 'w', header = vcf_in.header)
+vcf_out = VariantFile(sys.argv[2], 'w', header=vcf_in.header)
 
 for record in vcf_in.fetch():
     if record.filter.keys() == ["PASS"]:
