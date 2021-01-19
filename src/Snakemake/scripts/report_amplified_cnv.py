9f195e1309af5419fa0d1e954a334ca81d630224
Jonas Almlöf <>
2021-01-19 08:57:39 +0100

    Fixing input files for Filter_CNV.smk

--- src/Snakemake/scripts/report_amplified_cnv.py
+++ src/Snakemake/scripts/report_amplified_cnv.py
@@ -8,13 +8,14 @@ import subprocess
 
 cnv_purity = open(sys.argv[1])
 cnv_relevant_genes = open(sys.argv[2])
-cnv_files =  sys.argv[3:-3]
-cnv_ONCOCNV = open(sys.argv[-3])
-cnv_bed_file = open(sys.argv[-2])
+cnv_files =  sys.argv[3:-4]
+cnv_ONCOCNV = open(sys.argv[-4])
+cnv_bed_file = open(sys.argv[-3])
+raw_cnv_filename = sys.argv[-2]
 cnv_relevant = open(sys.argv[-1], "w")
 
 
-cnv_event = open("CNV_calls/cnv_raw_event.txt", "w")
+cnv_event = open(raw_cnv_filename, "w")
 
 cnv_relevant.write("sample_path\tsample\tgene\tchrom\tregion\tCNVkit_copy_ratio\tCN_CNVkit_100%\tCN_ONCOCNV_100%\tpurity\tCN_CNVkit\tCN_ONCOCNV\n")
 
@@ -118,7 +119,7 @@ cnv_purity.close()
 
 
 '''Find cnvs above cutoffs'''
-cnv_event = open("CNV_calls/cnv_raw_event.txt")
+cnv_event = open(raw_cnv_filename)
 for line in cnv_event :
     lline = line.strip().split("\t")
     regions = lline[4].split(",")
@@ -163,117 +164,117 @@ for line in cnv_event :
 cnv_relevant.close()
 
 
-'''Create plots'''
-for sample_file in cnv_files :
-    sample = sample_file.split(".cns")[0].split("/")[1]
-    sample2 = sample.split("-ready")[0]
-    path = sample_file.split("/")[0]
-    vcf = "Results/DNA/" + sample2 + "/vcf/" + sample2 + "-ensemble.final.vcf"
-    os.system("gunzip -c " + vcf + ".gz > " + vcf)
-    vcf_in = open(vcf)
-    vcf_out = open(vcf + ".rs", "w")
-    header = True
-    for line in vcf_in :
-        if header :
-            if line[:2] == "#C" :
-                header = False
-            vcf_out.write(line)
-            continue
-        lline = line.strip().split("\t")
-        rs = lline[2]
-        if rs != "." :
-            AD_index = 0
-            AF_index = 0
-            i = 0
-            for l in lline[8].split(":") :
-                if l == "AD" :
-                    AD_index = i
-                if l == "AF" :
-                    AF_index = i
-                i += 1
-            if len(lline[9].split(":")[AD_index].split(",")) == 2 :
-                if float(lline[9].split(":")[AF_index]) > 0.05 and float(lline[9].split(":")[AF_index]) < 0.95 :
-                    vcf_out.write(line)
-    vcf_in.close()
-    vcf_out.close()
-    #command_line = "/projects/wp4/nobackup/workspace/jonas_test/CNV_runs/20190909_JA_refernce/CNV/Helena/cnvkit-0.9.5/cnvkit.py scatter "
-    command_line = "singularity exec /projects/wp4/nobackup/workspace/somatic_dev/singularity/cnvkit_0.9.7--py_1.sif cnvkit.py scatter "
-    command_line += path + "/" + sample + ".cnr "
-    command_line += "-s " + path + "/" + sample + ".cns "
-    command_line += "-o CNV_results/" + sample + ".png "
-    command_line += "-v " + vcf + ".rs "
-    command_line += "--y-min -2 --y-max 2"
-    print(command_line)
-    os.system(command_line)
-    os.system("rm " + vcf)
-    #os.system("rm " + vcf + ".rs")
-
-cnv_relevant = open(sys.argv[-1])
-header = True
-for line in cnv_relevant :
-    if header :
-        header = False
-        continue
-    lline = line.strip().split("\t")
-    print(lline)
-    sample = lline[0].split("/")[1].split(".cns")[0]
-    sample2 = sample.split("-ready")[0]
-    path = lline[0].split("/")[0]
-    vcf = "Results/DNA/" + sample2 + "/vcf/" + sample2 + "-ensemble.final.vcf.rs"
-    gene = lline[2]
-    chrom = lline[3]
-    #gene_region = lline[4]
-    gene_regions_info = gene_regions[gene]
-    #gene_region1 = str(int(gene_regions_info[1])) + "-" + str(int(gene_regions_info[2]))
-    gene_region1 = str(max(int(gene_regions_info[1])-10000000,0)) + "-" + str(min(int(gene_regions_info[2])+10000000,chrom_len[chrom]))
-    gene_region2 = str(0) + "-" + str(chrom_len[chrom])
-    print(gene, gene_regions_info, gene_region1, gene_region2)
-    start_pos = int(gene_region1.split("-")[0])
-    end_pos = int(gene_region1.split("-")[1])
-
-    bed = open("DATA/TST500C_manifest.bed")
-    gene_string = ""
-    gene_name_dict = {}
-    for region in bed :
-        lregion = region.strip().split("\t")
-        #gene_name = lregion[3].split("_")[0]
-        exon = lregion[3]
-        if exon.find(gene + "_Exon") != -1 :
-        #if gene_name not in gene_name_dict :
-        #    gene_name_dict[gene_name] = ""
-            s_pos = int(lregion[1])
-            e_pos = int(lregion[2])
-            if (s_pos >= start_pos and e_pos <= end_pos) :
-                if gene_string == "" :
-                    gene_string = exon
-                    #gene_string = gene_name
-                else :
-                    gene_string += ","
-                    gene_string += exon
-                    #gene_string += gene_name
-    bed.close()
-    #command_line = "/projects/wp4/nobackup/workspace/jonas_test/CNV_runs/20190909_JA_refernce/CNV/Helena/cnvkit-0.9.5/cnvkit.py scatter "
-    command_line = "singularity exec /projects/wp4/nobackup/workspace/somatic_dev/singularity/cnvkit_0.9.7--py_1.sif cnvkit.py scatter "
-    command_line += path + "/" + sample + ".cnr "
-    command_line += "-s " + path + "/" + sample + ".cns "
-    command_line += "-c " + chrom + ":" + gene_region1
-    command_line += " -g " + gene_string
-    command_line += " -v " + vcf
-    command_line += " --title '" + sample + " " + chrom + " " + gene_region1 + " " + gene + "'"
-    command_line += " -o CNV_results/" + sample + "_" + gene + "_" + chrom + ":" + gene_region1 + ".png"
-    print(command_line)
-    os.system(command_line)
-    #command_line = "/projects/wp4/nobackup/workspace/jonas_test/CNV_runs/20190909_JA_refernce/CNV/Helena/cnvkit-0.9.5/cnvkit.py scatter "
-    command_line = "singularity exec /projects/wp4/nobackup/workspace/somatic_dev/singularity/cnvkit_0.9.7--py_1.sif cnvkit.py scatter "
-    command_line += path + "/" + sample + ".cnr "
-    command_line += "-s " + path + "/" + sample + ".cns "
-    command_line += "-c " + chrom + ":" + gene_region2
-    command_line += " -g " + gene_string
-    command_line += " -v " + vcf
-    command_line += " --title '" + sample + " " + chrom + " " + gene + "'"
-    command_line += " -o CNV_results/" + sample + "_" + gene + "_" + chrom + ".png"
-    print(command_line)
-    os.system(command_line)
-
-cnv_done = open("CNV_results/cnv_done.txt", "w")
-cnv_done.close()
+# '''Create plots'''
+# for sample_file in cnv_files :
+#     sample = sample_file.split(".cns")[0].split("/")[1]
+#     sample2 = sample.split("-ready")[0]
+#     path = sample_file.split("/")[0]
+#     vcf = "Results/DNA/" + sample2 + "/vcf/" + sample2 + "-ensemble.final.vcf"
+#     os.system("gunzip -c " + vcf + ".gz > " + vcf)
+#     vcf_in = open(vcf)
+#     vcf_out = open(vcf + ".rs", "w")
+#     header = True
+#     for line in vcf_in :
+#         if header :
+#             if line[:2] == "#C" :
+#                 header = False
+#             vcf_out.write(line)
+#             continue
+#         lline = line.strip().split("\t")
+#         rs = lline[2]
+#         if rs != "." :
+#             AD_index = 0
+#             AF_index = 0
+#             i = 0
+#             for l in lline[8].split(":") :
+#                 if l == "AD" :
+#                     AD_index = i
+#                 if l == "AF" :
+#                     AF_index = i
+#                 i += 1
+#             if len(lline[9].split(":")[AD_index].split(",")) == 2 :
+#                 if float(lline[9].split(":")[AF_index]) > 0.05 and float(lline[9].split(":")[AF_index]) < 0.95 :
+#                     vcf_out.write(line)
+#     vcf_in.close()
+#     vcf_out.close()
+#     #command_line = "/projects/wp4/nobackup/workspace/jonas_test/CNV_runs/20190909_JA_refernce/CNV/Helena/cnvkit-0.9.5/cnvkit.py scatter "
+#     command_line = "singularity exec /projects/wp4/nobackup/workspace/somatic_dev/singularity/cnvkit_0.9.7--py_1.sif cnvkit.py scatter "
+#     command_line += path + "/" + sample + ".cnr "
+#     command_line += "-s " + path + "/" + sample + ".cns "
+#     command_line += "-o CNV_results/" + sample + ".png "
+#     command_line += "-v " + vcf + ".rs "
+#     command_line += "--y-min -2 --y-max 2"
+#     print(command_line)
+#     os.system(command_line)
+#     os.system("rm " + vcf)
+#     #os.system("rm " + vcf + ".rs")
+#
+# cnv_relevant = open(sys.argv[-1])
+# header = True
+# for line in cnv_relevant :
+#     if header :
+#         header = False
+#         continue
+#     lline = line.strip().split("\t")
+#     print(lline)
+#     sample = lline[0].split("/")[1].split(".cns")[0]
+#     sample2 = sample.split("-ready")[0]
+#     path = lline[0].split("/")[0]
+#     vcf = "Results/DNA/" + sample2 + "/vcf/" + sample2 + "-ensemble.final.vcf.rs"
+#     gene = lline[2]
+#     chrom = lline[3]
+#     #gene_region = lline[4]
+#     gene_regions_info = gene_regions[gene]
+#     #gene_region1 = str(int(gene_regions_info[1])) + "-" + str(int(gene_regions_info[2]))
+#     gene_region1 = str(max(int(gene_regions_info[1])-10000000,0)) + "-" + str(min(int(gene_regions_info[2])+10000000,chrom_len[chrom]))
+#     gene_region2 = str(0) + "-" + str(chrom_len[chrom])
+#     print(gene, gene_regions_info, gene_region1, gene_region2)
+#     start_pos = int(gene_region1.split("-")[0])
+#     end_pos = int(gene_region1.split("-")[1])
+#
+#     bed = open("DATA/TST500C_manifest.bed")
+#     gene_string = ""
+#     gene_name_dict = {}
+#     for region in bed :
+#         lregion = region.strip().split("\t")
+#         #gene_name = lregion[3].split("_")[0]
+#         exon = lregion[3]
+#         if exon.find(gene + "_Exon") != -1 :
+#         #if gene_name not in gene_name_dict :
+#         #    gene_name_dict[gene_name] = ""
+#             s_pos = int(lregion[1])
+#             e_pos = int(lregion[2])
+#             if (s_pos >= start_pos and e_pos <= end_pos) :
+#                 if gene_string == "" :
+#                     gene_string = exon
+#                     #gene_string = gene_name
+#                 else :
+#                     gene_string += ","
+#                     gene_string += exon
+#                     #gene_string += gene_name
+#     bed.close()
+#     #command_line = "/projects/wp4/nobackup/workspace/jonas_test/CNV_runs/20190909_JA_refernce/CNV/Helena/cnvkit-0.9.5/cnvkit.py scatter "
+#     command_line = "singularity exec /projects/wp4/nobackup/workspace/somatic_dev/singularity/cnvkit_0.9.7--py_1.sif cnvkit.py scatter "
+#     command_line += path + "/" + sample + ".cnr "
+#     command_line += "-s " + path + "/" + sample + ".cns "
+#     command_line += "-c " + chrom + ":" + gene_region1
+#     command_line += " -g " + gene_string
+#     command_line += " -v " + vcf
+#     command_line += " --title '" + sample + " " + chrom + " " + gene_region1 + " " + gene + "'"
+#     command_line += " -o CNV_results/" + sample + "_" + gene + "_" + chrom + ":" + gene_region1 + ".png"
+#     print(command_line)
+#     os.system(command_line)
+#     #command_line = "/projects/wp4/nobackup/workspace/jonas_test/CNV_runs/20190909_JA_refernce/CNV/Helena/cnvkit-0.9.5/cnvkit.py scatter "
+#     command_line = "singularity exec /projects/wp4/nobackup/workspace/somatic_dev/singularity/cnvkit_0.9.7--py_1.sif cnvkit.py scatter "
+#     command_line += path + "/" + sample + ".cnr "
+#     command_line += "-s " + path + "/" + sample + ".cns "
+#     command_line += "-c " + chrom + ":" + gene_region2
+#     command_line += " -g " + gene_string
+#     command_line += " -v " + vcf
+#     command_line += " --title '" + sample + " " + chrom + " " + gene + "'"
+#     command_line += " -o CNV_results/" + sample + "_" + gene + "_" + chrom + ".png"
+#     print(command_line)
+#     os.system(command_line)
+#
+# cnv_done = open("CNV_results/cnv_done.txt", "w")
+# cnv_done.close()
