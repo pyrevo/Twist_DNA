fde9a348db523a9da77c0ae22e2259894d609a68
Jonas Almlöf <>
2021-01-29 16:26:15 +0100

    Added missing python scripts

---
+++ src/scripts/python/Add_FFPE_column_to_vcf.py
@@ -0,0 +1,23 @@
+
+import sys
+
+infile = open(sys.argv[1])
+outfile = open(sys.argv[2], "w")
+
+header = True
+for line in infile :
+    if header :
+        if line[:6] == "#CHROM" :
+            header = False
+        outfile.write(line)
+        continue
+    lline=line.strip().split("\t")
+    larti = lline[7].split(";")[-2:]
+    parti = larti[0].split("pArtifact=")[1]
+    arti = larti[1].split("artiStatus")[1]
+    if arti[0] == "=" :
+        arti = arti[1:]
+    outfile.write(line.strip() + "\t" + parti + "\t" + arti + "\n")
+    
+infile.close()
+outfile.close()
