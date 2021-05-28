7d3ba03162360f021b731f30bfa1e16e9c40eae6
Jonas Almlöf <>
2021-05-28 16:22:29 +0200

    bugfix

--- src/scripts/python/Filter_cnv_Loh.py
+++ src/scripts/python/Filter_cnv_Loh.py
@@ -171,6 +171,9 @@ for region in Both_regions:
         '''change this'''
         if (region[9] < 1.5 and (region[2] == "chr1" or region[2] == "chr19")) :
             clinical_gene = "IDH1"
+            cnv_relevant_clinical.write(method + "\t" + region[1] + "\t" + clinical_gene + "\t" + region[2] + "\t" + region[3] + "\t" +
+                               region[4] + "\t" + str(region[9]) + "\t" + str(region[11]) + "\t" + str(region[6]) + "\t" + str(region[8]) + "\t" +
+                               str(nr_exons) + "\t" + nr_obs_cov + "\t" + nr_obs_baf + "\t" + str(region[7]) + "\n")
         if (found_gene and region[9] > 2.5) or (region[9] < 1.5 and region[2] == "chr1") :
             cnv_relevant_clinical.write(method + "\t" + region[1] + "\t" + clinical_gene + "\t" + region[2] + "\t" + region[3] + "\t" +
                                region[4] + "\t" + str(region[9]) + "\t" + str(region[11]) + "\t" + str(region[6]) + "\t" + str(region[8]) + "\t" +
