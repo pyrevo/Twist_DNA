254d023c552cef828796aa59eed4a32a2958bf16
Jonas Almlöf <>
2021-08-24 14:26:19 +0200

    Add background and artfiact checks to soft filtering.

---
+++ src/scripts/python/Soft_artifact_filter.py
@@ -0,0 +1,53 @@
+
+in_vcf = open(snakemake.input.vcf)
+artifacts = open(snakemake.params.artifacts)
+out_vcf = open(snakemake.output.vcf, "w")
+
+
+artifact_dict = {}
+next(artifacts)
+for line in artifacts:
+    lline = line.strip().split("\t")
+    chrom = lline[0]
+    pos = lline[1]
+    type = lline[2]
+    observations = lline[3]
+    artifact_dict[chrom + "_" + pos] = [type, observations]
+
+
+header = True
+for line in in_vcf :
+    if header:
+        if line[:6] == "#CHROM":
+            out_vcf.write(line)
+            header = False
+        continue
+    lline = line.strip().split("\t")
+    chrom = lline[0]
+    pos = lline[1]
+    key = chrom + "_" + pos
+    ref = lline[3]
+    alt = lline[4]
+    filter = lline[6]
+    Observations = 0
+    if len(ref) == 1 and len(alt) == 1:
+        if key in artifact_dict:
+            if artifact_dict[key][0] == "SNV" :
+                Observations = artifact_dict[key][1]
+    else:
+        if key in artifact_dict:
+            if artifact_dict[key][0] == "INDEL" :
+                Observations = artifact_dict[key][1]
+    if Observations > 0 :
+        if filter == "PASS" :
+            filter = "Arti=" + str(Observations)
+        else :
+            filter += "|Arti=" + str(Observations)
+        lline[6] = filter
+        out_vcf.write(lline[0])
+        for l in lline[1:] :
+            out_vcf.write("\t" + l)
+        out_vcf.write("\n")
+    else :
+        out_vcf.write(line)
+out_vcf.close()
---
+++ src/scripts/python/Soft_background_filter.py
@@ -0,0 +1,64 @@
+
+in_vcf = open(snakemake.input.vcf)
+background_panel_filename = snakemake.params.background_panel
+out_vcf = open(snakemake.output.vcf, "w")
+
+
+background_panel_dict = {}
+if background_panel_filename != "" :
+    background_panel = open(background_panel_filename)
+    next(background_panel)
+    for line in background_panel:
+        lline = line.strip().split("\t")
+        chrom = "chr" + lline[0]
+        pos = lline[1]
+        median = float(lline[2])
+        sd = float(lline[3])
+        background_panel_dict[chrom + "_" + pos] = [type, observations]
+
+
+header = True
+for line in in_vcf :
+    if header:
+        if line[:6] == "#CHROM":
+            out_vcf.write(line)
+            header = False
+        continue
+    lline = line.strip().split("\t")
+    chrom = lline[0]
+    pos = lline[1]
+    key = chrom + "_" + pos
+    ref = lline[3]
+    alt = lline[4]
+    filter = lline[6]
+    INFO = lline[7]
+    if INFO[:3] == "AA=":
+        continue
+    INFO_list = INFO.split(";")
+    AF_index = 0
+    Caller_index = 0
+    i = 0
+    for info in INFO_list:
+        if info[:3] == "AF=":
+            AF_index = i
+        if info[:8] == "CALLERS=":
+            Caller_index = i
+        i += 1
+    AF = float(INFO_list[AF_index][3:])
+    nr_SD = 1000
+    if len(ref) == 1 and len(alt) == 1:
+        if key in background_panel_dict:
+            nr_SD = (AF - background_panel_dict[key][0]) / background_panel_dict[key][0]
+    if nr_SD < 10.0 :
+        if filter == "PASS" :
+            filter = "LownrSD=" + str(nr_SD)
+        else :
+            filter += "|LownrSD=" + str(nr_SD)
+        lline[6] = filter
+        out_vcf.write(lline[0])
+        for l in lline[1:] :
+            out_vcf.write("\t" + l)
+        out_vcf.write("\n")
+    else :
+        out_vcf.write(line)
+out_vcf.close()
